a87f6c40ca314f17f6d231287f6e66d4cd5c7151
Mahmoud Ali <mahmoud.ali.spider@gmail.com>
2025-08-04 15:16:44 +0300

    Remove unused Icon component and update favicon links in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -197,10 +197,10 @@ export default function RootLayout({
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" sizes="32x32" href="/favicon.ico" />
+        {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#36a1d7" />
+        {/* <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#36a1d7" /> */}
         <meta name="msapplication-TileColor" content="#36a1d7" />
         <meta name="theme-color" content="#36a1d7" />
       </head>
